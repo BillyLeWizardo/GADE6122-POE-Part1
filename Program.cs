d063030ee4d05c8f29caffccb9ac5d6a47fa1ab0
BillyLeWizardo <mr.williammhughes@gmail.com>
2022-10-19 01:05:27 +0200

    Made Program
    The beginning

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace GADE6122_POE_Part1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmARPG());
         }
     }
 }
