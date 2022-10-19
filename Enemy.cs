a966f2a5aad32132531d1ca0a18a63c9b7421788
OmegaOckie <ockiejnr@gmail.com>
2022-10-19 16:51:49 +0200

    Q.2.4

---
+++ Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6122_POE_Part1
+{
+    abstract internal class Enemy : Character
+    {
+        Random rng = new Random();
+
+        public Enemy(int enemyX, int enemyY, int enemyDamage, int enemyStartingHP, char enemySymbol) : base(enemyX,enemyY, enemySymbol)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "EnemyClassName at [X,Y] (Amount DMG)";
+        }
+    }
+}
