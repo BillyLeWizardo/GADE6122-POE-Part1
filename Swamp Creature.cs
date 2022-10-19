ea4253c2a96201c557253b4f42a5b6ef114d56cb
OmegaOckie <ockiejnr@gmail.com>
2022-10-19 17:19:01 +0200

    Q.2.5

--- Swamp Creature.cs
+++ Swamp Creature.cs
@@ -10,12 +10,28 @@ namespace GADE6122_POE_Part1
     {
         public Swamp_Creature(int swampCreatureX, int swampCreatureY) : base(swampCreatureX, swampCreatureY, 1, 10, 'S')
         {
-            
+
         }
 
         public override characterMovement ReturnMove(characterMovement move = characterMovement.NONE)
         {
-            rng
+            int randomDirection = rng.Next(5);
+            switch (randomDirection)
+            {
+                    case 0:
+                        return characterMovement.NONE;
+                    case 1:
+                        return characterMovement.UP;
+                    case 2:
+                        return characterMovement.DOWN;
+                    case 3:
+                        return characterMovement.LEFT;
+                    case 4:
+                        return characterMovement.RIGHT;
+                default:
+                    break;
+            }
+            throw new NotImplementedException();
         }
     }
 }
