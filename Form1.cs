f6b26770ea147d166741a6da814b6601d9e39fb4
BillyLeWizardo <mr.williammhughes@gmail.com>
2022-10-21 01:30:39 +0200

    Moving Onto Displaying in form
    :C Still too much man

--- Form1.cs
+++ Form1.cs
@@ -12,13 +12,9 @@ namespace GADE6122_POE_Part1
 {
     public partial class frmARPG : Form
     {
-        private Map gameMap;
-        public Map GameMap
         public frmARPG()
         {
             InitializeComponent();
-
-            gameMap = new Map(6, 15, 6, 15, 5);
         }
 
         private void frmARPG_Load(object sender, EventArgs e)
@@ -26,39 +22,9 @@ namespace GADE6122_POE_Part1
             refreshDisplay();
         }
 
-        public bool MovePlayer(Character.characterMovement direction)
-        {
-            switch (direction)
-            {
-                case Character.characterMovement.NONE:
-                    break;
-                case Character.characterMovement.UP:
-                    break;
-                case Character.characterMovement.DOWN:
-                    break;
-                case Character.characterMovement.LEFT:
-                    break;
-                case Character.characterMovement.RIGHT:
-                    break;
-                default:
-                    break;
-            }
-            return true;
-        }
-
         private void refreshDisplay()
         {
-            for (int y = 0; y < gameMap.mapHeight; y++)
-            {
-                for (int x = 0; x < gameMap.mapWidth; x++)
-                {
-                    TextBox txtBox = new TextBox();
-                    txtBox.Size = new Size(30, 30);
-                    txtBox.Name = "txt" + x.ToString() + "_" + y.ToString();
-                    txtBox.Location = new Point(x * 30, y * 30);
-                    pnlDisplay.Controls.Add(txtBox);
-                }
-            }
+            
         }
 
         private void btnLeft_Click(object sender, EventArgs e)
--- GameEngine.cs
+++ GameEngine.cs
@@ -8,16 +8,70 @@ namespace GADE6122_POE_Part1
 {
     public class GameEngine
     {
-        private Map gameMap { get; }
+        private Map gameMap;
+        public Map GameMap { get { return gameMap; } set { gameMap = value; } }
 
         public GameEngine()
         {
-            gameMap = new Map(10, 10, 10, 10, 5);
+            gameMap = new Map(10, 15, 10, 15, 5);
         }
 
-        public bool MovePlayer(Character.characterMovement direction)
+        public void movePlayer(Character.characterMovement direction)
         {
-            return true;
+            Map.TileMap[gameMap.Hero.TileY, gameMap.Hero.TileX] = new EmptyTile(gameMap.Hero.TileX, gameMap.Hero.TileY);
+
+            gameMap.Hero.Move(gameMap.Hero.ReturnMove(direction));
+
+            Map.TileMap[gameMap.Hero.TileY, gameMap.Hero.TileX] = gameMap.Hero;
+
+            gameMap.UpdateVision();
+        }
+
+        public void checkIfDead(Enemy enemy)
+        {
+            if (enemy.IsDead())
+            {
+                for (int i = 0; i < gameMap.totalEnemies - 1; i++)
+                {
+                    gameMap.Enemies[i] = gameMap.Enemies[i + 1];
+                    gameMap.Enemies[i].enemyArr -= 1;
+                }
+                gameMap.totalEnemies -= 1;
+
+                Map.TileMap[enemy.TileY, enemy.TileX] = new EmptyTile(enemy.TileX, enemy.TileY);
+            }
+        }
+
+        public void checkIfDead(Hero hero)
+        {
+            if (hero.IsDead())
+            {
+                Map.TileMap[hero.TileY,hero.TileX] = new EmptyTile(hero.TileX, hero.TileY);
+            }
+        }
+
+        public void moveAllEnemies()
+        {
+            for (int enemyCounter = 0; enemyCounter < gameMap.totalEnemies; enemyCounter++)
+            {
+                Map.TileMap[gameMap.Enemies[enemyCounter].TileY, gameMap.Enemies[enemyCounter].TileX] = new EmptyTile(gameMap.Enemies[enemyCounter].TileX, gameMap.Enemies[enemyCounter].TileY);
+
+                gameMap.Enemies[enemyCounter].Move(gameMap.Enemies[enemyCounter].ReturnMove());
+
+                gameMap.UpdateVision();
+            }
+        }
+
+        public void enemyAttack()
+        {
+            for (int enemyCounter = 0; enemyCounter < gameMap.totalEnemies; enemyCounter++)
+            {
+                if (gameMap.Enemies[enemyCounter].CheckRange(gameMap.Hero))
+                {
+                    gameMap.Enemies[enemyCounter].Attack(gameMap.Hero);
+                }
+            }
+            checkIfDead(gameMap.Hero);
         }
     }
 }
